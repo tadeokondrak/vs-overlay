8540004be520870f074cb1e782e3f56ae43141bc
Simon Bruder <simon@sbruder.de>
2021-01-22 15:13:45 +0100

    vapoursynthPlugins.beziercurve: switch to meson

--- plugins/beziercurve/default.nix
+++ plugins/beziercurve/default.nix
@@ -1,8 +1,6 @@
-{ stdenv, fetchFromGitHub, vapoursynth }:
+{ fetchFromGitHub, meson, ninja, pkg-config, stdenv, vapoursynth }:
 
-let
-  ext = stdenv.targetPlatform.extensions.sharedLibrary;
-in stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "vapoursynth-beziercurve";
   version = "r3";
 
@@ -13,25 +11,8 @@ in stdenv.mkDerivation rec {
     sha256 = "1513ndj7sxwihyxx6x9ciyd8jhw9vs6lhzw7fpl7cz7fdj49wwi6";
   };
 
+  nativeBuildInputs = [ meson ninja pkg-config ];
   buildInputs = [ vapoursynth ];
-
-  patchPhase = ''
-    substituteInPlace VapourSynth-BezierCurve/BezierCurve.h \
-        --replace '<vapoursynth\' '<vapoursynth/'
-  '';
-
-  buildPhase = ''
-    c++ -fPIC -shared -I${vapoursynth}/include/vapoursynth \
-        -o VapourSynth-BezierCurve${ext} \
-	      VapourSynth-BezierCurve/BezierCurve.cpp \
-        VapourSynth-BezierCurve/CubicBezierCurve.cpp \
-        VapourSynth-BezierCurve/QuadraticBezierCurve.cpp \
-        VapourSynth-BezierCurve/VapourSynth-BezierCurve.cpp
-  '';
-
-  installPhase = ''
-    install -D VapourSynth-BezierCurve${ext} $out/lib/vapoursynth/VapourSynth-BezierCurve${ext}
-  '';
 
   meta = with stdenv.lib; {
     description = "A bézier curve plugin for VapourSynth";
